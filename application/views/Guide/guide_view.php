85b09e51f3d18aafaad464450e28168f17043796
Akshay <akshaydesilva2002@gmail.com>
2024-05-04 15:41:14 +0530

    updated guide

--- application/views/Guide/guide_view.php
+++ application/views/Guide/guide_view.php
@@ -18,13 +18,47 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 </head>
 
-<body x-data="guide">
-<section class="bg-gray-50 dark:bg-gray-900">
+<nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+	<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+		<div class="flex items-center space-x-3 rtl:space-x-reverse">
+			<img src="https://png.pngtree.com/png-clipart/20230816/original/pngtree-radiation-symbol-of-activity-on-white-background-picture-image_7986533.png" class="h-8" alt="Logo">
+			<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">PDA NETWORK</span>
+		</div>
+		<div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+			<ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+				<li>
+					<a href="<?php echo base_url('home') ?>" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+						Home
+					</a>
+				</li>
+				<li>
+					<a href="<?php echo base_url('profile') ?>" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+						Profile
+					</a>
+				</li>
+				<li>
+					<a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+						Zone Updates
+					</a>
+				</li>
+				<li>
+					<a href="<?php echo base_url('guide') ?>" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+						Guide
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
+
+<body x-data="guide"
+	  class="bg-gray-50 dark:bg-gray-900">
+<section>
 	<div class="flex flex-col items-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-		<div class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+		<div class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white pt-24">
 			<img class="w-20 w-20 mr-2" src="https://png.pngtree.com/png-clipart/20230816/original/pngtree-radiation-symbol-of-activity-on-white-background-picture-image_7986533.png" alt="logo">
 		</div>
-		<div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+		<div class="w-3/4 bg-white rounded-lg shadow dark:border md:mt-0 xl:p-0 dark:bg-gray-800 dark:border-gray-700">
 			<div class="p-6 space-y-4 md:space-y-6 sm:p-8">
 				<div class="flex justify-center">
 					<h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
@@ -32,44 +66,79 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					</h1>
 				</div>
 
-				<?php if($this->session->flashdata('success')): ?>
-					<div class="bg-green-100 border-l-4 border-green-500 text-green-700 p-4" role="alert">
-						<p class="font-bold">Success</p>
-						<p><?php echo $this->session->flashdata('success'); ?></p>
-					</div>
-				<?php endif; ?>
-
-				<?php if($this->session->flashdata('error')): ?>
-					<div class="bg-red-100 border-l-4 border-red-500 text-red-700 p-4" role="alert">
-						<p class="font-bold">Error</p>
-						<p><?php echo $this->session->flashdata('error'); ?></p>
-					</div>
-				<?php endif; ?>
-
 				<div class="flex flex-col space-y-1">
-					<label for="search" class="text-sm font-semibold text-gray-600 dark:text-gray-300">Search</label>
 					<input
 						x-model="searchText"
 						type="text" name="search" id="search" class="block w-full p-3 text-sm dark:bg-gray-800 dark:text-gray-300 border border-gray-200 rounded focus:outline-none focus:border-blue-400" placeholder="Search for a guide...">
 				</div>
 				<button
 					@click="search()"
-					type="button" class="w-full p-3 mt-4 text-sm font-semibold text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">Search</button>
+					type="button" class="w-full p-3 mt-4 text-sm font-semibold text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
+					Search for Guide(s)
+				</button>
+
+				<template x-if="guideResults.length > 0">
+
+					<button
+						@click="guideResults = []"
+						type="button" class="w-full p-3 mt-4 text-sm font-semibold text-white bg-red-400 rounded-md hover:bg-red-600 focus:outline-none focus:bg-red-600">
+						Clear Result(s)
+					</button>
+				</template>
+
+				<div class="flex justify-center mt-4">
+					<p class="text-sm font-semibold text-gray-600 dark:text-gray-300">Results found: <span x-text="guideResults.length"></span></p>
+				</div>
 
 				<div class="flex flex-col space-y-4">
 					<template x-if="guideResults.length > 0">
-						<template x-for="guide in guideResults">
-							<div class="flex flex-col space-y-2" :key="guide.entry_id">
-								<div class="flex items center justify-between">
-									<h2 class="text-lg font-semibold text-gray-900 dark:text-white" x-text="guide.entry_name"></h2>
-									<p class="text-sm font-semibold text-gray-600 dark:text-gray-300" x-text="guide.entry_category"></p>
-									<p class="text-sm font-semibold text-gray-600 dark:text-gray-300" x-text="guide.entry_description"></p>
+
+						<div class="dark:bg-gray-800">
+							<div class="mx-auto max-w-7xl px-6 lg:px-8">
+								<div class="mx-auto max-w-2xl lg:max-w-4xl">
+									<div class="mt-16 space-y-20 lg:mt-20 lg:space-y-20">
+
+										<template x-for="guide in guideResults" :key="guide.entry_id">
+
+											<article class="relative isolate flex flex-col gap-8 lg:flex-row">
+												<div class="relative aspect-[16/9] sm:aspect-[2/1] lg:aspect-square lg:w-64 lg:shrink-0">
+													<img src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80" alt="" class="absolute inset-0 h-full w-full rounded-2xl bg-gray-50 object-cover">
+													<div class="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
+												</div>
+												<div>
+													<div class="group relative max-w-xl">
+														<h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+															<div class="text-[#ffbc00]">
+																<span class="absolute inset-0"></span>
+																<div x-text="guide.entry_name"></div>
+															</div>
+														</h3>
+														<p class="mt-5 text-sm leading-6 text-white" x-text="guide.entry_description"></p>
+													</div>
+													<div class="mt-6 flex border-t border-gray-900/5 pt-6">
+														<div class="relative flex items-center gap-x-4">
+															<div class="text-sm leading-6">
+																<p class="font-semibold text-gray-900">
+																	<div class="text-white">
+																		<span class="absolute inset-0"></span>
+																		Category: <p x-text="guide.entry_category"></p>
+																	</div>
+																</p>
+															</div>
+														</div>
+													</div>
+												</div>
+											</article>
+										</template>
+										<template x-if="guideResults.length <= 0">
+											<p class="text-sm font-semibold text-gray-600 dark:text-gray-300">No results found.</p>
+										</template>
+
+									</div>
 								</div>
 							</div>
-						</template>
-					</template>
-					<template x-if="guideResults.length <= 0">
-						<p class="text-sm font-semibold text-gray-600 dark:text-gray-300">No results found.</p>
+						</div>
+
 					</template>
 				</div>
 			</div>
