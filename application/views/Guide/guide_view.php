1ae8b34de079d27d0c5acc3fe6407569b66ced1b
Akshay <akshaydesilva2002@gmail.com>
2024-05-11 19:45:41 +0530

    changed guide view from alpine to backbone

--- application/views/Guide/guide_view.php
+++ application/views/Guide/guide_view.php
@@ -11,11 +11,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<!-- Imports Tailwind CSS by its Official CDN -->
 	<script src="https://cdn.tailwindcss.com"></script>
 
-	<!-- Alpine JS Javascript framework CDN -->
-	<script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-
-	<!-- Axios CDN -->
+	<!-- Axios CDN. Used for AJAX Requests -->
 	<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+
+	<!-- Underscore CDN -->
+	<!-- According to the Backbone.js documentation, this library is a dependency. -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.13.6/underscore-min.js" integrity="sha512-2V49R8ndaagCOnwmj8QnbT1Gz/rie17UouD9Re5WxbzRVUGoftCu5IuqqtAM9+UC3fwfHCSJR1hkzNQh/2wdtg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+	<!-- jQuery CDN -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+	<!-- Backbone.js CDN -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.6.0/backbone-min.js" integrity="sha512-ei5TeAaO5TpzrvI9Y0NP+/gr6cfcF9wmCnuXEXuwLfTsyspAlBjwGSSVkQbZsA8wDC5fEKufEHgMmJ/HPNWlAw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </head>
 
 <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
@@ -60,7 +67,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	}
 </style>
 
-<body x-data="guide"
+<body
+	id="guide"
+	x-data="guide"
 	  class="bg-gray-50 dark:bg-gray-900">
 <section>
 	<div class="flex flex-col items-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -77,86 +86,35 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 				<div class="flex flex-col space-y-1">
 					<input
-						x-model="searchText"
+						id="searchText"
 						type="text" name="search" id="search" class="block w-full p-3 text-sm dark:bg-gray-800 dark:text-gray-300 border border-gray-200 rounded focus:outline-none focus:border-blue-400" placeholder="Search for a guide...">
 				</div>
+
 				<button
-					@click="await $nextTick(); search()"
+					id="searchButton"
 					type="button" class="w-full p-3 mt-4 text-sm font-semibold text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
 					Search for Guide(s)
 				</button>
 
 				<button
-					@click="await $nextTick(); showAll()"
-					type="button" class="w-full p-3 mt-4 text-sm font-semibold text-white bg-green-500 rounded-md hover:bg-green-600 focus:outline-none focus:bg-green-600">
-					Display all Guides
+					id="clearButton"
+					type="button" class="hidden w-full p-3 mt-4 text-sm font-semibold text-white bg-red-400 rounded-md hover:bg-red-600 focus:outline-none focus:bg-red-600">
+					Clear Result(s)
 				</button>
 
-				<template x-if="guideResults.length > 0">
-
-					<button
-						@click="await $nextTick(); guideResults = []"
-						type="button" class="w-full p-3 mt-4 text-sm font-semibold text-white bg-red-400 rounded-md hover:bg-red-600 focus:outline-none focus:bg-red-600">
-						Clear Result(s)
-					</button>
-				</template>
-
-				<div class="flex justify-center mt-4">
-					<p class="text-sm font-semibold text-gray-600 dark:text-gray-300">Results found: <span x-text="guideResults.length"></span></p>
-				</div>
-
 				<div class="flex flex-col space-y-4">
-					<template x-if="guideResults.length > 0">
 
-						<div class="dark:bg-gray-800">
-							<div class="mx-auto max-w-7xl px-6 lg:px-8">
-								<div class="mx-auto max-w-2xl lg:max-w-4xl">
-									<div class="mt-16 space-y-20 lg:mt-20 lg:space-y-20">
-
-										<template x-for="guide in guideResults" :key="guide.entry_id">
-
-											<article class="relative isolate flex flex-col gap-8 lg:flex-row">
-												<div class="relative aspect-[16/9] sm:aspect-[2/1] lg:aspect-square lg:w-64 lg:shrink-0">
-													<img
-														:src="guide.entry_image_path"
-														alt="" class="absolute inset-0 h-full w-full rounded-2xl bg-gray-50 object-cover">
-													<div class="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
-												</div>
-												<div>
-													<div class="group relative max-w-xl">
-														<h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-															<div class="text-[#ffbc00]">
-																<span class="absolute inset-0"></span>
-																<div x-text="guide.entry_name"></div>
-															</div>
-														</h3>
-														<p class="mt-5 text-sm leading-6 text-white" x-text="guide.entry_description"></p>
-													</div>
-													<div class="mt-6 flex border-t border-gray-900/5 pt-6">
-														<div class="relative flex items-center gap-x-4">
-															<div class="text-sm leading-6">
-																<p class="font-semibold text-gray-900">
-																	<div class="text-white">
-																		<span class="absolute inset-0"></span>
-																		Category: <p x-text="guide.entry_category"></p>
-																	</div>
-																</p>
-															</div>
-														</div>
-													</div>
-												</div>
-											</article>
-										</template>
-										<template x-if="guideResults.length <= 0">
-											<p class="text-sm font-semibold text-gray-600 dark:text-gray-300">No results found.</p>
-										</template>
-
+					<div class="dark:bg-gray-800">
+						<div class="mx-auto max-w-7xl px-6 lg:px-8">
+							<div class="mx-auto max-w-2xl lg:max-w-4xl">
+								<div class="mt-16 space-y-20 lg:mt-20 lg:space-y-20">
+									<div class="results-container">
+										<!-- Results will be displayed here -->
 									</div>
 								</div>
 							</div>
 						</div>
-
-					</template>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -166,47 +124,129 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 </html>
 
 <script>
-	document.addEventListener('alpine:init', () => {
-		Alpine.data('guide', () => ({
-			open: false,
-			searchText : '',
 
-			guideResults: [],
+	// Backbone.js Model
+	var Guide = Backbone.Model.extend({
+		defaults: {
+			'searchText': '',
+			'guideResults': [],
+		}
+	});
 
-			// triggered when the page is loaded
-			init() {
-				console.log('Guide page loaded.');
-			},
+	// Backbone.js View
+	var GuideView = Backbone.View.extend({
+		el: '#guide',
 
-			search() {
-				axios.post('<?php echo base_url('Guide/GuideController/search') ?>', {
-					search: this.searchText
-				},{
-					headers: {
-						'Content-Type': 'application/x-www-form-urlencoded'
-					}
-				})
-				.then(response => {
-					console.log('success from controller request');
+		model: new Guide(),
 
-					if(response.data === false) {
-						this.guideResults = [];
-						return;
-					}
+		events: {
+			'click #searchButton': 'search',
+			'input #searchText': 'updateSearchText',
+			'click #clearButton': 'clearResults',
+		},
 
-					this.guideResults = response.data;
-				})
-				.catch(error => {
-					console.log('error');
-					console.log(error);
-				})
-			},
+		initialize: function() {
+			console.log('Guide page loaded.');
 
-			showAll() {
-				this.searchText = '';
-				this.search();
-			},
+			this.listenTo(this.model, 'change:guideResults', this.render);
+		},
 
-		}))
-	})
+		updateSearchText: function(e) {  // Add this function
+			this.model.set('searchText', e.target.value);
+		},
+
+		clearResults: function() {
+			this.model.set('searchText', '');
+			this.model.set('guideResults', []);
+		},
+
+		search: function() {
+			console.log('searching for guide(s)....');
+
+			axios.post('<?php echo base_url('Guide/GuideController/search') ?>', {
+				search: this.model.get('searchText')
+			},{
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded'
+				}
+			})
+			.then(response => {
+				console.log('success from controller request');
+
+				if(response.data === false) {
+					this.model.set('guideResults', []);
+					return;
+				}
+
+				this.model.set('guideResults', response.data);
+			})
+			.catch(error => {
+				console.log('An error ocurred... Please check the console for more information.');
+				console.log(error);
+			})
+		},
+
+		render: function() {
+			var results = this.model.get('guideResults');
+			var resultsContainer = this.$el.find('.results-container');
+
+			resultsContainer.empty();
+
+			if (results.length === 0) {
+
+				resultsContainer.append('<p class="text-sm font-semibold text-gray-600 dark:text-gray-300">No results found.</p>');
+
+				// hide the clear button
+				document.getElementById('clearButton').classList.add('hidden');
+
+			} else {
+
+				// This loops through the results and appends them to the results container
+				results.forEach(function(guide) {
+
+					var guideElement = '<article class="relative isolate flex flex-col gap-8 lg:flex-row my-6">' +
+						'<div class="relative aspect-[16/9] sm:aspect-[2/1] lg:aspect-square lg:w-64 lg:shrink-0">' +
+						'<img src="' + guide.entry_image_path + '" alt="" class="absolute inset-0 h-full w-full rounded-2xl bg-gray-50 object-cover">' +
+						'<div class="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>' +
+						'</div>' +
+						'<div>' +
+						'<div class="group relative max-w-xl">' +
+						'<h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">' +
+						'<div class="text-[#ffbc00]">' +
+						'<span class="absolute inset-0"></span>' +
+						'<div>' + guide.entry_name + '</div>' +
+						'</div>' +
+						'</h3>' +
+						'<p class="mt-5 text-sm leading-6 text-white">' + guide.entry_description + '</p>' +
+						'</div>' +
+						'<div class="mt-6 flex border-t border-gray-900/5 pt-6">' +
+						'<div class="relative flex items-center gap-x-4">' +
+						'<div class="text-sm leading-6">' +
+						'<p class="font-semibold text-gray-900">' +
+						'<div class="text-white">' +
+						'<span class="absolute inset-0"></span>' +
+						'Category: <p>' + guide.entry_category + '</p>' +
+						'</div>' +
+						'</p>' +
+						'</div>' +
+						'</div>' +
+						'</div>' +
+						'</div>' +
+						'</article>';
+
+					resultsContainer.append(guideElement);
+
+					// unhide the clear button
+					document.getElementById('clearButton').classList.remove('hidden');
+
+				});
+			}
+		},
+	});
+
+	// Initialize the view object AFTER the page has loaded
+	window.onload = function() {
+		new GuideView();
+	};
+
 </script>
